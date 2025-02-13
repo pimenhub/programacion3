5fab7c9d610c61f7e5c4cd290058feb40a18c233
pimenhub <pimenteleduca@gmail.com>
2025-02-12 23:23:09 -0600

    memoria

---
+++ Memoria/src/Arreglo.java
@@ -0,0 +1,16 @@
+public class Arreglo {
+    public int[] arregloEntero(){
+        int[] enteros = new int[10];
+        enteros[0] = 1;
+        enteros[1] = 1;
+        enteros[2] = 1;
+        enteros[3] = 1;
+        enteros[4] = 1;
+        enteros[5] = 1;
+        enteros[6] = 1;
+        enteros[7] = 1;
+        enteros[8] = 1;
+        enteros[9] = 1;
+        return enteros;
+    }
+}
---
+++ Memoria/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Arreglo a = new Arreglo();
+        System.out.println(a.arregloEntero().length);
+        System.out.println(a.arregloEntero()[2]);
+    }
+
+
+}
